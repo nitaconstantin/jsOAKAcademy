acf2b344471410068e5dc1ac8e990174d5616a42
Nita Constantin <ctin.nita88@gmail.com>
2023-08-09 11:11:30 +0300

    Javascript Language Basics - Lectia 2 Type Coercion

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log("Hello from separated file!");
+
+/* 
+Basic JavaScript Data Types:
+1. String:It is used for text, shown between quotation marks
+2. Number: Integers, Decimal numbers, Percentages
+3. Boolean: True or False
+4. Udefined: Data type of a variable that does not yet have a value
+5. Null: nonexistent
+
+*/
+var name = "Alex";
+console.log(name);
+
+var schoolTeam = "basketball";
+var schoolNumber = 20;
+console.log(schoolNumber);
+
+var isRegistered = true;
+console.log(isRegistered);
+
+// Type Coercion
+var job;
+console.log(job);
+job = "student";
+console.log(job);
+
+console.log(name + " " + schoolNumber);
+
+var schoolName, hasCourse;
+schoolName = "OAKAcademy";
+hasCourse;
+console.log(
+  schoolName + " has 35 courses. Does it have any course?" + hasCourse
+);
